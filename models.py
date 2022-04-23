b44d6752460093f7e172e2ea2977708fddf89556
SHAJU <87312839+shaju47@users.noreply.github.com>
2022-04-23 20:25:25 +0530

    yeah modifiedtest

--- models.py
+++ models.py
@@ -4,5 +4,5 @@ from django.db import models
 class build(models.Model):
     name=models.CharField(max_length=20, blank=False, null=False, primary_key=True)
     password=models.CharField()
-    mail=models.EmailField(null=False, blank=False)
+    mail=models.EmailField(null=False)
     phone=models.ImageField(null=False, blank=True)
