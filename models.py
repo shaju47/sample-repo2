b19efa5fefca7c9a96bd075e473cf17367a4d1aa
SHAJU <shajuga110@gmail.com>
2022-04-23 18:58:24 +0530

    i added a modelspython file

---
+++ models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class build(models.Model):
+    name=models.CharField(max_length=20, blank=False, null=False, primary_key=True)
+    password=models.CharField(max_length=20,blank=False, null=False)
+    mail=models.EmailField(null=False, blank=False)
+    phone=models.ImageField(null=False, blank=False)
