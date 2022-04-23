844a1d82c5b5d322c6d63ae4d17c677046266a52
SHAJU <shajuga110@gmail.com>
2022-04-23 19:53:31 +0530

    modified the brancgh new

--- models.py
+++ models.py
@@ -5,4 +5,4 @@ class build(models.Model):
     name=models.CharField(max_length=20, blank=False, null=False, primary_key=True)
     password=models.CharField(max_length=20,blank=False, null=False)
     mail=models.EmailField(null=False, blank=False)
-    phone=models.ImageField(null=False, blank=False)
+    phone=models.ImageField(null=False, blank=True)
